ebce4d4c8bb61baae5901d5dc8e1b9ecdf8753d1
zhangmei <15713586837@163.com>
2024-07-04 01:34:18 +0800

    fixï: windows 拉取代码

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+const add = () => {
+	count.value++;
+};
+</script>
 
 <template>
-  <div class="a">app</div>
+	<div class="a" @click="add">app{{ count }}</div>
 </template>
 
 <style lang="scss" scoped>
 .a {
 	color: $color-theme;
+	font-size: 12px;
+	width: 100%;
+	height: 100%;
 }
 </style>
