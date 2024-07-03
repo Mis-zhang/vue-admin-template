e818ff05a161b88f31df71f5f6ce8c67c298fab8
zhangmei123 <zhangmei@bendiyun.cn>
2024-07-03 18:59:15 +0800

    fix: 添加打包分析依赖、通过监听文件修改，自动重启 vite 服务插件

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 打包分析依赖
+import { visualizer } from 'rollup-plugin-visualizer'
+// 通过监听文件修改，自动重启 vite 服务
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    visualizer({ open: true }),
+    ViteRestart({ restart: ['vite.config.[jt]s'] }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
