45ca081ecb4b3408a1b42bb4323d0485ed2ec0ad
zhangmei123 <zhangmei@bendiyun.cn>
2024-07-04 14:47:40 +0800

    feat: 执行 prettier 命令

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 // 打包分析依赖
-import { visualizer } from 'rollup-plugin-visualizer'
+import { visualizer } from 'rollup-plugin-visualizer';
 // 通过监听文件修改，自动重启 vite 服务
-import ViteRestart from 'vite-plugin-restart'
+import ViteRestart from 'vite-plugin-restart';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    visualizer({ open: true }),
-    ViteRestart({ restart: ['vite.config.[jt]s'] }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  // 引入sass全局样式变量
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/var.scss";`,
-      },
-    },
-  },
-})
+	plugins: [vue(), vueDevTools(), visualizer({ open: true }), ViteRestart({ restart: ['vite.config.[jt]s'] })],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	// 引入sass全局样式变量
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "@/styles/var.scss";`,
+			},
+		},
+	},
+});
