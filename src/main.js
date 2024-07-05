e09c306c7fa0616cc5ef2912c9b07352ac58c7fd
zhangmei <15713586837@163.com>
2024-07-05 17:37:32 +0800

    feat: 导入 pinia 状态管理库，以及状态持久化库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 // 导入 vue-router
 import router from '@/router';
+// 导入 pinia 状态管理库
+import pinia from '@/stores';
 // 设置 Arco 组件全局一些配置
 import { Card, Modal } from '@arco-design/web-vue';
 // 导入 Arco Ui 库 自定义样式
@@ -21,4 +23,5 @@ Card.props.bordered = false;
 Modal._context = app._context;
 
 app.use(router);
+app.use(pinia);
 app.mount('#app');
