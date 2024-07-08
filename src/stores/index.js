9e691fb6d5d0f15beafefda89e02494b160992b4
zhangmei <15713586837@163.com>
2024-07-08 18:57:49 +0800

    feat: store 导入 modules js

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,9 @@
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
+export * from './modules/app';
+export * from './modules/dict';
+
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
